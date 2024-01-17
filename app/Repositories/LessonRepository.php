fb604e3cf072bf1f2d6473edc35c5e2ca4be7e93
Andrade <daniloasf@outlook.com>
2024-01-17 12:00:34 -0300

    Adicionar métodos ao LessonRepository

--- app/Repositories/LessonRepository.php
+++ app/Repositories/LessonRepository.php
@@ -10,4 +10,44 @@ class LessonRepository
     {
         $this->entity = $lesson;
     }
+
+    public function getLessonsModule(int $moduleId)
+    {
+        return $this->entity
+                        ->where('module_id', $moduleId)
+                        ->get();
+    }
+
+    public function createNewLesson(int $moduleId, array $data)
+    {
+        $data['module_id'] = $moduleId;
+
+        return $this->entity->create($data);
+    }
+
+    public function getLessonByModule(int $moduleId, $identify)
+    {
+        return $this->entity
+                        ->where('module_id', $moduleId)
+                        ->where('uuid', $identify)
+                        ->firstOrFail();
+    }
+
+    public function updateLessonByUuid(int $moduleId, string $identify, array $data)
+    {
+        $lesson = $this->entity
+                            ->where('uuid', $identify)
+                            ->get();
+
+        $data['module_id'] = $moduleId;
+
+        return $lesson->update($data);
+    }
+
+    public function deleteLessonByUuid(string $identify)
+    {
+        $lesson = $this->entity->where('uuid', $identify);
+
+        return $lesson->delete();
+    }
 }
