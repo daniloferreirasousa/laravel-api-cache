d5340315562e4a408787a74d29e70bffbc088915
Andrade <daniloasf@outlook.com>
2024-01-19 08:59:34 -0300

    Criar Factory para trabalhar com Testes PHP

---
+++ database/factories/LessonFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lesson;
+use App\Models\Module;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Lesson>
+ */
+class LessonFactory extends Factory
+{
+    protected $model = Lesson::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        $name = $this->faker->unique()->name();
+
+        return [
+            'module_id' => Module::factory(),
+            'name' => $name,
+            'video' => $name,
+            'description'   => $this->faker->sentence(10),
+        ];
+    }
+}
